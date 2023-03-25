cc48945a5e28b1cb44e707c01e7bff8da45f7fac
Trqz <joaomarcostroquez@gmail.com>
2023-03-25 22:14:24 +0000

    ADDED high camera

--- project_watermelon/Assets/Scripts/CharacterHigh.cs
+++ project_watermelon/Assets/Scripts/CharacterHigh.cs
@@ -2,11 +2,15 @@ using FMODUnity;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Cinemachine;
 
 public class CharacterHigh : MonoBehaviour
 {
     public float highAmmount;
     public float highDecreaseRate = 2;
+    public float cameraShakeIncrease = 10;
+    [SerializeField]
+    private CinemachineFreeLook highCamera;
     // Start is called before the first frame update
 
     // Update is called once per frame
@@ -15,7 +19,12 @@ public class CharacterHigh : MonoBehaviour
         if (highAmmount > 0)
         {
             RuntimeManager.StudioSystem.setParameterByName("High", highAmmount);
+            highCamera.Priority = 11;
             highAmmount -= Time.deltaTime * highDecreaseRate;
+        }
+        else
+        {
+            highCamera.Priority = 9;
         }
     }
 }
