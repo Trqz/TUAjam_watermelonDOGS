0f453554f93402b4d20b74b5207666eeec70c004
ShootDawn <72670900+ShootDawn@users.noreply.github.com>
2023-03-25 04:37:15 +0000

    sons de cao

--- project_watermelon/Assets/Scripts/CharacterControllerInput.cs
+++ project_watermelon/Assets/Scripts/CharacterControllerInput.cs
@@ -1,5 +1,8 @@
+using FMOD.Studio;
+using FMODUnity;
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditorInternal;
 using UnityEngine;
 
 public class CharacterControllerInput : MonoBehaviour
@@ -11,20 +14,84 @@ public class CharacterControllerInput : MonoBehaviour
     [SerializeField]
     private float rotateSpeed;
 
+    [Header("SFX Stuff")]
+    [SerializeField] EventReference walkingSFX;
+    EventInstance walkingI;
+    bool walkingStarted;
+    [SerializeField] EventReference breathingSFX;
+    EventInstance breathingI;
+    bool breathingStarted;
+    private float breathingTimer;
+    [SerializeField] float breathingNeed = 10;
+
     void Start()
     {
         _characterController = GetComponent<CharacterController>();
+
+        //Start Sounds
+        walkingI = RuntimeManager.CreateInstance(walkingSFX);
+        breathingI = RuntimeManager.CreateInstance(breathingSFX);
     }
 
     void Update()
     {
-        movementInput = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+        movementInput = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical")).normalized;
         movementInput = Quaternion.Euler(0, Camera.main.transform.eulerAngles.y, 0) * movementInput;
         //movementInput = transform.TransformDirection(movementInput);
         _characterController.Move(movementInput * movementSpeed * Time.deltaTime);
 
         transform.LookAt(new Vector3(movementInput.x, 0, movementInput.z));
+        movementSFX();
 
 
+    }
+
+    void movementSFX()
+    {
+        Debug.Log(breathingTimer);
+        if (movementInput.magnitude > 0 )
+        {
+            if (!walkingStarted)
+            {
+                walkingI.start();
+                walkingStarted = true;
+            }
+            if (!breathingStarted)
+            {
+
+
+
+                breathingI.start();
+                breathingStarted = true;
+            }
+            if(breathingTimer < 100)
+            {
+                breathingTimer += Time.deltaTime * breathingNeed;
+                breathingI.setParameterByName("TimeWalking", breathingTimer);
+            }
+        } else
+        {
+            if (walkingStarted)
+            {
+                walkingI.stop(FMOD.Studio.STOP_MODE.ALLOWFADEOUT);
+                walkingStarted = false;
+            }
+            if (breathingStarted) { 
+            breathingI.stop(FMOD.Studio.STOP_MODE.ALLOWFADEOUT);
+            breathingStarted=false;
+            }
+            if (breathingTimer >= 0)
+            {
+                breathingTimer -= Time.deltaTime * breathingNeed * 5;
+                breathingI.setParameterByName("TimeWalking", breathingTimer);
+            }
+        }
+    }
+    private void OnDisable()
+    {
+        walkingI.stop(FMOD.Studio.STOP_MODE.IMMEDIATE);
+        walkingI.release();
+        breathingI.stop(FMOD.Studio.STOP_MODE.IMMEDIATE);
+        breathingI.release();
     }
 }
