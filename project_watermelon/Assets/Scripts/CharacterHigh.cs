898bddf248c0bb265746e23e6dffedd97580d2e9
Trqz <joaomarcostroquez@gmail.com>
2023-03-26 02:33:16 +0100

    ADDED drugged animation

--- project_watermelon/Assets/Scripts/CharacterHigh.cs
+++ project_watermelon/Assets/Scripts/CharacterHigh.cs
@@ -11,7 +11,13 @@ public class CharacterHigh : MonoBehaviour
     public float cameraShakeIncrease = 10;
     [SerializeField]
     private CinemachineFreeLook highCamera;
+    private Animator _animator;
+
     // Start is called before the first frame update
+    private void Start()
+    {
+        _animator = GetComponent<Animator>();
+    }
 
     // Update is called once per frame
     void Update()
@@ -21,10 +27,12 @@ public class CharacterHigh : MonoBehaviour
             RuntimeManager.StudioSystem.setParameterByName("High", highAmmount);
             highCamera.Priority = 11;
             highAmmount -= Time.deltaTime * highDecreaseRate;
+            _animator.SetBool("isDrugged", true);
         }
         else
         {
             highCamera.Priority = 9;
+            _animator.SetBool("isDrugged", false);
         }
     }
 }
